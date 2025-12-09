adb6d0c5affe5b82731ab60e638080d49c65beb8
Elenzoo <sergiuszzyromski@gmail.com>
2025-12-09 16:48:26 +0100

    Add startup log for HEADLESS_BROWSER

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // index.js
 import { startWatcher } from "./watcher.js";
 
+console.log("[CFG] HEADLESS_BROWSER (process.env) =", process.env.HEADLESS_BROWSER);
+
 startWatcher().catch((err) => {
   console.error("Fatal error:", err);
   process.exit(1);
